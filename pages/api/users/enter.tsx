8a3b89640d321863a7b415d2d2a2d5abf38f04f5
GJ <lgj9172@gmail.com>
2022-12-01 16:30:25 +0900

    add: paths

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -1,5 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import withHandler from "../../../libs/server/withHandler";
+import withHandler from "@libs/server/withHandler";
+import client from "@libs/server/client";
 
 async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.body);
